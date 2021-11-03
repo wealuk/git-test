47b0f41fc76a135f271a9363158bd6c27c670ff5
“wealuk” <2546867299@qq.com>
2021-11-03 21:53:10 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2021-11-03 21:46
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
